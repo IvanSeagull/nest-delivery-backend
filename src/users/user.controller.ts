0d5842e46aea00dad216f2ab8614dd3568846a55
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-03-26 23:54:17 +0000

    test

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from "@nestjs/common";
 import { usersService } from "./user.service";
 
 @Controller('users')
-export class UserController{
-    constructor( private readonly userService: usersService){}
+export class UserController {
+    constructor(private readonly userService: usersService) { }
 
     @Get()
-    getUsers(){
-        return 123
+    getUsers() {
+        return "Test"
     }
 }
