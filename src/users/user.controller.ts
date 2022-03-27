6ffeaf774b9db606b9f5352ad8e9d7a1248a202b
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-03-27 00:14:51 +0000

    created register function

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { usersService } from "./user.service";
 
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: usersService) { }
 
-    @Get()
-    getUsers() {
-        return "Test"
+    @Post()
+    registerUsers(@Body("username") username: string, @Body("password") password:string, @Body('repeatPassword') repeatPassword:string) {
+        if(password === repeatPassword) {   
+            const newUserId = this.userService.registerUser(username, password)
+            return newUserId;
+        }
+        else{
+            return {msg: "Passwords dont match"}
+        }
     }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { User } from "./user.model";
 
 @Injectable()
-export class usersService{}
+export class usersService{
+    private users: User[] = [];
+
+    registerUser(username: string, password: string){
+        const id = this.users.length + 1;
+        const newUser = new User(id, username, password);
+        this.users.push(newUser)
+        return id
+    }
+
+}
